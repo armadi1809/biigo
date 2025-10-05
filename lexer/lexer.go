278cdce985657913ea74e814119ad7a41e74af6f
Aziz Rmadi <azizrmadi@gmail.com>
2025-10-05 13:58:18 +0200

    lexer: skip whitespaces, handle new lines and two chars operators

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -66,6 +66,39 @@ func (lexer *Lexer) scanToken() error {
 		lexer.addToken(token.SEMICOLON, "")
 	case '*':
 		lexer.addToken(token.STAR, "")
+	case '!':
+		if lexer.match('=') {
+			lexer.addToken(token.BANG_EQUAL, "")
+		} else {
+			lexer.addToken(token.BANG, "")
+		}
+	case '<':
+		if lexer.match('=') {
+			lexer.addToken(token.LESS_EQUAL, "")
+		} else {
+			lexer.addToken(token.LESS, "")
+		}
+	case '>':
+		if lexer.match('=') {
+			lexer.addToken(token.GREATER_EQUAL, "")
+		} else {
+			lexer.addToken(token.GREATER, "")
+		}
+	case '/':
+		if lexer.match('/') {
+			for lexer.peek() != '\n' && !lexer.isAtEnd() {
+				lexer.advance()
+			}
+		} else {
+			lexer.addToken(token.SLASH, "")
+		}
+	case ' ':
+	case '\r':
+	case '\t':
+		break
+	case '\n':
+		lexer.line += 1
+
 	default:
 		err = &langerror.LangError{
 			Line:    lexer.line,
@@ -77,10 +110,32 @@ func (lexer *Lexer) scanToken() error {
 
 func (lexer *Lexer) advance() int32 {
 	lexer.current += 1
-	return []rune(lexer.source)[lexer.current-1]
+	return lexer.getCharAtPos(lexer.current - 1)
 }
 
 func (lexer *Lexer) addToken(tok token.TokenType, literal string) {
 	lexeme := lexer.source[lexer.start:lexer.current]
 	lexer.tokens = append(lexer.tokens, *token.NewToken(tok, lexeme, literal, lexer.line))
+}
+
+func (lexer *Lexer) match(expected int32) bool {
+	if lexer.isAtEnd() {
+		return false
+	}
+	if lexer.getCharAtPos(lexer.current) != expected {
+		return false
+	}
+	lexer.current += 1
+	return true
+}
+
+func (lexer *Lexer) peek() int32 {
+	if lexer.isAtEnd() {
+		return '\000'
+	}
+	return lexer.getCharAtPos(lexer.current)
+}
+
+func (lexer *Lexer) getCharAtPos(pos int) int32 {
+	return []rune(lexer.source)[pos]
 }
